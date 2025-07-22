5aa10bc1f4e27df5bd661ad323a3c7c15e175f9a
Dhruba <gdhruba748+github@gmail.com>
2025-07-22 18:41:46 +0530

    added comment for smallest number

--- warm up - day 3/largest-number-array.js
+++ warm up - day 3/largest-number-array.js
@@ -1,11 +1,11 @@
 // Write a function that returns the largest number in an array
 
-let arr = [1, 12, 13, -4, 5, 16, 7, 98, 9];
+let arr = [1, 12, 13, -4, 5, 16, 733, 98, 9];
 
 function largestNumber(arr) {
   let num = arr[0];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > num) num = arr[i];
+    if (arr[i] > num) num = arr[i]; //for smallest do arr[i] < num
   }
   return num;
 }
