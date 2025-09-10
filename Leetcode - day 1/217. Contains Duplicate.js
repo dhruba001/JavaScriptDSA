3d7c4ffe10ade5c527d280e7d0909323a9e87c8e
Dhruba <gdhruba748+github@gmail.com>
2025-09-10 13:12:31 +0530

    latest

--- Leetcode - day 1/217. Contains Duplicate.js
+++ Leetcode - day 1/217. Contains Duplicate.js
@@ -44,3 +44,14 @@ Constraints:
 var containsDuplicate = function (nums) {
   return new Set(nums).size !== nums.length;
 };
+
+var constainsDuplicate2 = function (nums) {
+  const seen = new Set();
+  for (let num of nums) {
+    if (seen.has(num)) {
+      return true;
+    }
+    seen.add(num);
+  }
+  return false;
+};
