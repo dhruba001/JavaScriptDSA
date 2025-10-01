2c3fbc867188df96bfbf72f7e41ec3c25164f284
Dhruba <gdhruba748+github@gmail.com>
2025-10-01 16:44:35 +0530

    Leetcode will be a different folder

---
+++ LeetCode/leetcode - day 6/347. Top K Frequent Elements.js
@@ -0,0 +1,54 @@
+/*
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+
+Output: [1,2]
+
+Example 2:
+
+Input: nums = [1], k = 1
+
+Output: [1]
+
+Example 3:
+
+Input: nums = [1,2,1,2,1,2,3,1,3,2], k = 2
+
+Output: [1,2]
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+*/
+
+var topKFrequent = function (nums, k) {
+  const count = {};
+  const freq = Array.from({ length: nums.length + 1 }, () => []);
+
+  for (const n of nums) {
+    count[n] = (count[n] || 0) + 1;
+  }
+  for (const n in count) {
+    freq[count[n]].push(parseInt(n));
+  }
+
+  const res = [];
+  for (let i = freq.length - 1; i > 0; i--) {
+    for (const n of freq[i]) {
+      res.push(n);
+      if (res.length === k) {
+        return res;
+      }
+    }
+  }
+};
